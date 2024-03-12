5a5a3938cc6738c5d2185494ae349e5b69d42ec7
Domodan <ongomdaniel9@gmail.com>
2024-03-12 18:43:43 +0000

    updated the routers and controllers

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-
 import UsersController from '../controllers/UsersController';
-/*import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
-*/
 
 function Routes(app) {
   const router = express.Router();
